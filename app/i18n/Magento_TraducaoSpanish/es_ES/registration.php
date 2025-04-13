9c0ac75dc2db8724e7fe7b4202d7b0a121884951
Thiago Reis <thiagoaurea@gmail.com>
2025-04-13 00:17:49 -0300

    Create registration.php

---
+++ app/i18n/Magento_TraducaoSpanish/es_ES/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magento_traducaospanish_es_es',
+    __DIR__
+);
